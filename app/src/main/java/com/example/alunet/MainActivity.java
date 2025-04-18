7660c8de287cd1a175573fe0792bebe853c2a99d
rocharenata <renatarocha95@hotmail.com>
2025-04-17 23:51:51 -0300

    Primeiro commit após resetar

---
+++ app/src/main/java/com/example/alunet/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.alunet;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.text.SpannableString;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Set the status bar color to transparent
+        getWindow().setStatusBarColor(getResources().getColor(android.R.color.transparent, getTheme()));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Handle button click
+    }
+
+    text.view.setText(new SpannableString("ALU NET"));
+    }
+}
